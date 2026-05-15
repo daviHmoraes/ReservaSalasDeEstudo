70e297ba5d71c3cb54467135154d8f75ea7cd4f4
Arthur Jardim <arthur_jardim-souza@estudante.sesisenai.org.br>
2026-05-15 19:07:12 -0300

    feat(sala): adiciona entidade sala

---
+++ src/model/entity/Sala.java
@@ -0,0 +1,58 @@
+package model.entity;
+
+public class Sala {
+
+    private int id;
+    private int numero;
+    private int capacidade;
+    private boolean disponivel;
+
+    public Sala (int id, int numero, int capacidade, boolean disponivel){
+
+        this.id = id;
+        this.numero = numero;
+        this.capacidade = capacidade;
+        this.disponivel = disponivel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void exixbirDados(){
+        System.out.println( "ID: " + id );
+        System.out.println("Número: " + numero);
+        System.out.println("Capacidade: " + capacidade);
+        System.out.println("Status: " + disponivel);
+
+    }
+}
+
