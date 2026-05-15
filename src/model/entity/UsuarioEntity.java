90e04d73f1e58af4e12667a3ff00457fc8a124aa
luiz_go_barcelos <luiz_go_barcelos@estudante.sesisenai.org.br>
2026-05-15 18:57:54 -0300

    feat(usuario): adiciona entidade usuario

---
+++ src/model/entity/UsuarioEntity.java
@@ -0,0 +1,53 @@
+package model.entity;
+
+public class UsuarioEntity {
+
+    private String nome;
+    private int id;
+    private int idPlus;
+    private String cpf;
+
+    public UsuarioEntity(String nome, int id, int idPlus, String cpf) {
+        this.nome = nome;
+        this.id = idPlus++;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdPlus() {
+        return idPlus;
+    }
+
+    public void setIdPlus(int idPlus) {
+        this.idPlus = idPlus;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void exibirDados(){
+        System.out.println("ID: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+    }
+}
