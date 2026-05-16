78ab8cc678a58846442b43146a04ce8c35450aa7
jardim <jardima62@gmail.com>
2026-05-16 14:46:50 -0300

    feat(sala) adiciona métodos atualizar remover e listar

--- src/model/service/SalaService.java
+++ src/model/service/SalaService.java
@@ -3,12 +3,15 @@ package model.service;
 import model.entity.Sala;
 import model.repository.SalaRepository;
 
+import java.util.HashMap;
 
 public class SalaService {
 
+    private SalaRepository salaRepository;
+
     public SalaService() {
 
-        SalaRepository salaRepository;
+        salaRepository = new SalaRepository();
 
     }
 
@@ -19,5 +22,71 @@ public class SalaService {
             return false;
 
         }
+
+        //verifica se já existe sala com esse ID
+        if (salaRepository.buscarID(sala.getId()) != null) {
+
+            System.out.println("Já existe uma sala cadastrada com esse ID. ");
+            return false;
+
+        }
+
+        salaRepository.salvar(sala);
+
+        System.out.println();
+
+        System.out.println("Sala cadastrada com sucesso. ");
+        return true;
     }
+
+    //listar
+    public HashMap<Integer, Sala> listarSalas() {
+
+        return salaRepository.listarSala();
+
+    }
+
+    //atualizar
+    public boolean atualizarSala(Sala salaAtualizada) {
+
+        if (salaRepository.buscarID(salaAtualizada.getId()) == null) {
+
+            System.out.println("Sala não encontrada. ");
+            return false;
+
+        }
+
+        if (salaAtualizada.getCapacidade() <= 0) {
+
+            System.out.println("Capacidade inválida. ");
+            return false;
+
+
+        }
+
+        salaRepository.atualizar(salaAtualizada);
+
+        System.out.println("Sala atualizada com sucesso. ");
+        return true;
+
+    }
+
+    //remover
+    public boolean removerSala(int id){
+
+        if (salaRepository.buscarID(id) == null){
+
+            System.out.println("Sala não encontrada. ");
+            return false;
+
+        }
+
+        salaRepository.removerSala(id);
+
+        System.out.println("Sala removida com sucesso. ");
+
+        return true;
+
+    }
+
 }
