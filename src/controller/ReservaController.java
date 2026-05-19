000b6fbcf123a8345fbd3542701d6fcb2965615c
Davi Heinzen <davi_h_moraes@estudante.sesisenai.org.br>
2026-05-19 20:16:23 -0300

    feat(reserva): adiciona controller de reserva

---
+++ src/controller/ReservaController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.entity.Reserva;
+import model.service.ReservaService;
+
+public class ReservaController {
+
+    private ReservaService reservaService = new ReservaService();
+
+    public void cadastrar(Reserva reserva, int idUsuario, int idSala) {
+        reservaService.cadastrar(reserva, idUsuario, idSala);
+    }
+
+    public void listar() {
+        reservaService.listar();
+    }
+
+    public Reserva buscarPorId(int id) {
+        return reservaService.buscarPorId(id);
+    }
+
+    public void atualizar(Reserva reserva) {
+        reservaService.atualizar(reserva);
+    }
+
+    public void remover(Reserva reserva) {
+        reservaService.remover(reserva);
+    }
+
+}
